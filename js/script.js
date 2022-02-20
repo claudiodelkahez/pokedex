baabf5cc585fb0539020bef47c68126f1f052770
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-02-19 21:26:16 -0300

    fetched data display on DOM

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const poke_container = document.getElementById('poke-container')
-const pokemon_count = 150
+const pokemon_count = 250
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -17,6 +17,9 @@ const colors = {
     normal: '#F5F5F5'
 }
 
+const main_types = Object.keys(colors)
+console.log(main_types)
+
 const fetchPokemon = async () => {
     for (let i = 1; i <= pokemon_count; i++) {
         await getPokemon(i)
@@ -26,10 +29,42 @@ const fetchPokemon = async () => {
 
 const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-    const res = await fetch (url)
+    const res = await fetch(url)
     const data = await res.json()
-    console.log(data)
+    createPokemonCard(data)
 
 }
+const createPokemonCard = (pokemon) => {
+    const pokemonElement = document.createElement('div')
+    pokemonElement.classList.add('pokemon')
 
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+
+    const id = pokemon.id.toString().padStart(3, '0')
+
+    const poke_types = pokemon.types.map(type => type.type.name)
+
+    const type = main_types.find(type => poke_types.indexOf(type) > -1)
+
+    const color = colors[type]
+
+    pokemonElement.style.backgroundColor = color
+
+    const pokemonInnerHTML = `
+    <div class="img-container">
+        <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
+            alt="${name}"
+        />
+    </div>
+    <div class="info">
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span></small>
+    </div>`
+
+    pokemonElement.innerHTML = pokemonInnerHTML
+
+    poke_container.appendChild(pokemonElement)
+}
 fetchPokemon()
